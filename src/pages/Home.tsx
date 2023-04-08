287a9d88f38d0f00eecbfc0e6839cafd9e0e4378
nearworld <artemismars2@outlook.com>
2023-04-08 16:57:07 +0900

    feat: routes 설정 및 main_header 컴포넌트 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainHeader from "../components/main.header";
+
+const Home = () => {
+  return (
+    <section>
+      <MainHeader />
+    </section>
+  );
+};
+
+export default Home;
